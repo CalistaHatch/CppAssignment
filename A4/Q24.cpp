976d0b24e63037dd3494541d47eba54bbcb494c6
CalistaHatch <71110328+CalistaHatch@users.noreply.github.com>
2020-10-08 10:08:41 -0700

    Create Q24.cpp

---
+++ A4/Q24.cpp
@@ -0,0 +1,8 @@
+Q24. Write a function that accept one positive integer argument and returns true if the argument is a prime number, or false otherwise. Write two test cases in the main function to verify the function you wrote.
+
+For the definition of prime number, you can refer to the wikipedia: https://en.wikipedia.org/wiki/Prime_number
+
+Further explanation of the prime number:
+
+7 is prime because it can only be evenly divided by 1 and 7.
+4 is not prime because it can be divided evenly by 1, 2, 4.
