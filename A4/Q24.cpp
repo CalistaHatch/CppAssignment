a067f0bc717ae8cc2d0f778992db2493ac15a699
CalistaHatch <71110328+CalistaHatch@users.noreply.github.com>
2020-10-11 14:40:18 -0700

    Update Q24.cpp

--- A4/Q24.cpp
+++ A4/Q24.cpp
@@ -6,3 +6,31 @@ Further explanation of the prime number:
 
 7 is prime because it can only be evenly divided by 1 and 7.
 4 is not prime because it can be divided evenly by 1, 2, 4.
+//Calista Hatch
+  
+  #include <iostream>
+using namespace std;
+ 
+int main()
+{
+  int n, i;
+  bool Num_is_Prime = true;
+ 
+  cout << "Enter a positive number:  ";
+  cin >> n;
+ 
+  for(i = 2; i <= n / 2; ++i)
+  {
+      if(n % i == 0)
+      {
+          Num_is_Prime = false;
+          break;
+      }
+  }
+  if (Num_is_Prime)
+      cout <<n<<" is a prime number "<<endl;
+  else
+      cout <<n<<" is not a prime number"<<endl;
+ 
+  return 0;
+}
