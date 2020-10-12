df75fc26dd803930a4a3ccd183456118874e500c
CalistaHatch <71110328+CalistaHatch@users.noreply.github.com>
2020-10-11 18:11:41 -0700

    Update Q23.cpp

--- A4/Q23.cpp
+++ A4/Q23.cpp
@@ -1,6 +1,5 @@
-Q23. In a program, write a function that accepts three arguments: an array, the size of the array, 
-and a number n . Assume that the array contains integers. The function should display all of the numbers in the array 
-that are greater than the number n. Write two test cases in the main function to verify the function you wrote.
+//Q23
+// Calista Hatch
 
 #include<iostream>
 
