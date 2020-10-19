43521c6726513bd5d25dc8a126441405ab80b030
CalistaHatch <71110328+CalistaHatch@users.noreply.github.com>
2020-10-18 20:21:24 -0700

    Update Q16.cpp

--- A5/Q16.cpp
+++ A5/Q16.cpp
@@ -1,9 +1,5 @@
-Q16. Write a function that accepts an int array and the array’s size as arguments. The function should create a copy of the array, except that the element values should be reversed in the copy. The function should return a pointer to the new array. Demonstrate the function in a complete program.
-
-Hint:
-
-Use Dynamic Memory Allocation
-The original array should not be modified.
+//Q16.
+//Calista Hatch
 
   #include <iostream>
 using namespace std;
