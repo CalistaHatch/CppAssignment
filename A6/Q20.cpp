230d101e044ff528aa107b7ba4e02342a85fa333
CalistaHatch <71110328+CalistaHatch@users.noreply.github.com>
2020-10-25 10:31:15 -0700

    Create Q20.cpp

---
+++ A6/Q20.cpp
@@ -0,0 +1,4 @@
+Q20. Write a class declaration named Cylinder with a private member variable named radius and height . Write set and get functions to access the radius and height variables, and a function named getVolume that returns the volume of the Cylinder. The volume is calculated as
+
+3.14159 * radius * radius * height
+Use test cases in main function to test your class.
