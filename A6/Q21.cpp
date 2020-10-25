ecef2361f0fee5435200e56ed062a7a5f326f9f7
CalistaHatch <71110328+CalistaHatch@users.noreply.github.com>
2020-10-25 10:31:44 -0700

    Create Q21.cpp

---
+++ A6/Q21.cpp
@@ -0,0 +1,3 @@
+Q21. Design a PayRoll class that has data members for an employee’s hourly pay rate, number of hours worked, and total pay for the week. Write a program with an array of seven PayRoll objects. The program should ask the user for the number of hours each employee has worked and will then display the amount of gross pay each has earned.
+
+Please conduct Input Validation and do not accept values greater than 60 for the number of hours worked.
