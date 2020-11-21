8bbf1af3220310c04990e5069e4b841df04cb990
CalistaHatch <71110328+CalistaHatch@users.noreply.github.com>
2020-11-20 18:54:11 -0800

    Create Q5.cpp

---
+++ A9/Q5.cpp
@@ -0,0 +1,32 @@
+Q5. Fill the blanks to complete the program
+
+// This program demonstrates a unique_ptr pointing to a dynamically allocated array of integers.
+
+#include <iostream>
+#include <memory>
+using namespace std;
+
+int main()
+ {
+  int max; // Max size of the array
+
+  // Ask the user to enter an integer, and store the integer to the variable  int
+
+  *Code goes here*
+  
+  // Define a unique_ptr smart pointer, pointing to a dynamically allocated array of ints. The length of the array is equal to the value of max. 
+
+  *Code goes here*
+
+ // Ask the user to enter intergers for each value of the int array. 
+
+  *Code goes here*
+
+
+// Display all the values in the array.
+
+  *Code goes here*
+
+ return 0;
+ }
+
