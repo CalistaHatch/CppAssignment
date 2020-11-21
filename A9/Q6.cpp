35bdd4ae0e85828ee87f8c6b86cf4183222192eb
CalistaHatch <71110328+CalistaHatch@users.noreply.github.com>
2020-11-20 18:54:30 -0800

    Create Q6.cpp

---
+++ A9/Q6.cpp
@@ -0,0 +1,4 @@
+Q6. Write a class named StockPrices . The class constructor should accept an array of stock prices as its argument.
+The class should have a member function that returns the average of the stock prices. If any stock price in the array 
+is negative or greater than 5000, the class should throw an exception. Demonstrate the class in a program.
+
