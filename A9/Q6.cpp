ff4f65d4b502b104f7008a7712887f114cdbda0c
CalistaHatch <71110328+CalistaHatch@users.noreply.github.com>
2020-11-22 14:34:57 -0800

    Update Q6.cpp

--- A9/Q6.cpp
+++ A9/Q6.cpp
@@ -1,4 +1,46 @@
-Q6. Write a class named StockPrices . The class constructor should accept an array of stock prices as its argument.
-The class should have a member function that returns the average of the stock prices. If any stock price in the array 
-is negative or greater than 5000, the class should throw an exception. Demonstrate the class in a program.
-
+//Q6. 
+  
+#include <iostream>
+using namespace std;
+//class 
+class Stockprices
+{
+    public:
+    int prices[7];//prices array 
+    Stockprices(int arr[])//constructor that accepts a Stockprices array as argument 
+    {
+        for(int i=0;i<7;++i)
+               prices[i] = arr[i];
+    }
+    //find average 
+    int average()
+    {
+        int sum=0,avg=0;
+         for(int i=0;i<7;++i){
+             //if prices greater than 5000 and negative throw an exception 
+          if(prices[i]>5000 || prices[i]<0)
+            throw "stock price is not a valid!";
+            
+            sum+=prices[i]; 
+         }
+         avg=sum/7;
+         return avg;//return average value 
+    }
+};
+int main()
+{ //array containing Stockprices 
+    int arr[7]={2000,3100,300,700,4070,3456,867};
+    Stockprices obj(arr);//create object and constructor with argument
+    
+   //exception handling  
+    try {
+        int ans=obj.average();
+    cout<<ans;
+      
+   } catch (const char* error_msg) {
+     cerr << error_msg << endl;
+   }
+    
+   
+    return 0;
+}
