9ee4a970164ee5291cbb2fe60ebb85e42b363bb0
CalistaHatch <71110328+CalistaHatch@users.noreply.github.com>
2020-12-06 15:23:53 -0800

    Update Q7.cpp

--- A10/Q7.cpp
+++ A10/Q7.cpp
@@ -1,8 +1,37 @@
-Q7. Write a program that asks the user for the name of a file, and an integer number. The program 
-should display the first several lines of the file on the screen, and the number of lines should 
-be equal to the integer entered by the user. If the number of lines the file has is less than the
-integer entered by the user, then the entire file should be displayed, with a message indicating 
-the entire file has been displayed.
+//Q7. 
+#include <iostream>
+#include <fstream>
+using namespace std;
 
-When evaluating the peer submissions, please consider two cases to verify the program, with one 
-case where the file has enough lines, and one case where the file does not have enough lines.
+int main()
+{
+/*make a object*/
+ifstream finish;
+/*declare the variable*/
+string line,file;
+int n;
+/*take input from user*/
+cout<<"Enter file name:";
+cin>>file;
+cout<<"Enter the number of line:";
+cin>>n;
+/*open a file*/
+finish.open(file);
+while(finish && n>0)
+{
+// Read a Line from file
+getline(finish, line);
+// Write line in Console
+cout << line << endl;
+n--;
+}
+finish.close();
+
+if(n!=0)
+
+{
+cout<<endl<<"The complete file has been displayed \n";
+
+}
+return 0;
+}
